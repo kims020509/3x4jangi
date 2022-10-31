6b9b9dccb2a6b8577fe4db2f8b0f0e156fad2ca9
ZiLA3 <kms-0509@naver.com>
2022-10-31 14:48:19 +0900

    4.221031
    확인 골재 작성

--- BOARD.py
+++ BOARD.py
@@ -5,6 +5,7 @@ class BOARD:
         self.board = np.array([['--', '--', '--', '--'] for i in range(3)])
         self.piece_xy = np.array(['b10', 'a10', 'c10', 'b20', 'x00', 'b41', 'a41', 'c41', 'b31', 'x00'])
         self.piece_type = np.array(['K', 'S', 'J', 'Z', 'H', 'K', 'S', 'J', 'Z', 'H'])
+        self.piece_go = np.loadtxt('piece_go.csv', bool, delimiter=',')
         self.y_axis = {'a' : 0, 'b' : 1, 'c' : 2}
 
     def print(self):
@@ -24,22 +25,34 @@ class BOARD:
             print_piece = self.piece_type[i] + self.piece_xy[i][2]
             self.board[y][x] = print_piece
 
+    def check_xy(self, new_xy, old_xy):
+        dx = [1, 0, -1, 1, 0, -1, 1, 0, -1]
+        dy = [1, 1, 1, 0, 0, 0, -1, -1, -1]
+        new_xy[0]
+
     def solve_command(self, command):
         type_dic = {'K' : 0, 'S' : 1, 'J' : 2, 'Z' : 3, 'H' : 4}
-        arr = list()
-        for i in command:
-            arr.append(i)
+        arr = list(i for i in command)
+        
         if arr[1] in self.y_axis:
             type_num = type_dic[arr[0]]
-            new_xy = arr[1] + arr[2] + '0'
-            self.piece_xy[type_num] = new_xy
+            new_xy_num = (int(arr[2]), self.y_axis[arr[1]])
+            old_xy_num = (int(self.piece_xy[type_num][1]), self.y_axis[self.piece_xy[type_num][0]])
+            print(new_xy_num, old_xy_num)
+            new_xy_txt = arr[1] + arr[2] + '0'
+            self.piece_xy[type_num] = new_xy_txt
 
 if __name__ == '__main__':
     playing = BOARD()
     playing.reset_board()
     playing.print()
-    playing.solve_command(input())
-    playing.reset_board()
-    playing.print()
+    while 1:
+        command = input()
+        if command == '0':
+            break
+        playing.solve_command(command)
+        playing.reset_board()
+        playing.print()
+    
 
     
