3b29ea03047c1277d91f264a61ae4c27a1f064a9
ZiLA3 <kms-0509@naver.com>
2022-11-24 09:11:16 +0900

    11.221124
    움직임 구현

--- BOARD.py
+++ BOARD.py
@@ -4,18 +4,18 @@ class BOARD:
     def __init__(self):
         self.board = np.array([['--', '--', '--', '--'] for i in range(3)])
         self.type_dic = {'K' : 0, 'S' : 1, 'J' : 2, 'Z' : 3, 'H' : 4}
-        self.piece_xy = np.array(['b10', 'a10', 'c10', 'b20', 'x00', 'b41', 'a41', 'c41', 'b31', 'x00'])
+        self.piece_xy = np.array(['b10', 'a10', 'c10', 'b20', 'x00', 'b41', 'a41', 'c41', 'b31', 'x01'])
         self.piece_type = np.array(['K', 'S', 'J', 'Z', 'H', 'K', 'S', 'J', 'Z', 'H'])
         self.piece_go = np.loadtxt('piece_go.csv', bool, delimiter=',')
-        self.y_axis = {'a' : 1, 'b' : 2, 'c' : 3}
+        self.y_axis = {'a' : 1, 'b' : 2, 'c' : 3, 1 : 'a', 2 : 'b', 3 : 'c'}
         self.turn = 0
 
     def print(self):
         print('-'*11)
         print('AI : ', end='')
-        for i in range(5):
-            if self.piece_xy[i][0] == '@':
-                print(self.piece_type[i], end=' ')
+        for num, i in enumerate(self.piece_xy):
+            if i[0] == '@' and i[2] == '1':
+                print(self.piece_type[num], end=' ')
         print()
         print('-'*11)
         for y in range(3):
@@ -24,9 +24,9 @@ class BOARD:
             print()
         print('-'*11)
         print('PL : ', end='')
-        for i in range(4, 10):
-            if self.piece_xy[i][0] == '@':
-                print(self.piece_type[i], end=' ')
+        for num, i in enumerate(self.piece_xy):
+            if i[0] == '@' and i[2] == '0':
+                print(self.piece_type[num], end=' ')
         print()
         print('-'*11)
 
@@ -52,6 +52,17 @@ class BOARD:
     def getBoardNow(self, xy):
         return self.board[xy[1] - 1][xy[0] - 1]
 
+    def getNum(self, xy):
+        board = self.getBoardNow(xy)
+        type_num = self.type_dic[board[0]]
+        print(type_num)
+        for i in range(2):
+            key = (self.piece_xy[type_num][1], self.piece_xy[type_num][0])
+            key = (int(key[0]), self.y_axis[key[1]])
+            print(xy, key)
+            if xy == key: return type_num
+            type_num += 5
+
     def isIn(self, xy):
         if xy[0] > 4 or xy[1] > 3: return False
         return True
@@ -63,22 +74,32 @@ class BOARD:
     def isCanGo(self, new_xy, old_xy, type_num, turn):
         nx, ny = (new_xy[0] - old_xy[0], new_xy[1] - old_xy[1])
         index = self.getlocalXY(nx, ny)
-        if not self.piece_go[type_num][index]: return False
+        if not self.piece_go[type_num % 5][index]: return False
         if self.getBoardNow(new_xy)[1] == turn: return False
         return True
 
     def move(self, new_xy, type_num, turn):
         old_xy = (int(self.piece_xy[type_num][1]), self.y_axis[self.piece_xy[type_num][0]])
         if self.isCanGo(new_xy, old_xy, type_num, turn):
-            new_xy_txt = self.y_axis[new_xy[1]] + new_xy[0] + turn
-            self.piece_xy[type_num] = new_xy_txt
+            if not self.isEmpty(new_xy):
+                other_num = self.getNum(new_xy)
+                print(other_num)
+                if other_num % 5 == 4:
+                    self.piece_xy[other_num - 1] = '@0' + turn
+                    self.piece_xy[other_num] = 'x00'
+                else:
+                    self.piece_xy[other_num] = '@0' + turn
+            new_xy_txt = self.y_axis[new_xy[1]] + f'{new_xy[0]}' + turn
+
             if type_num % 5 == 3 and new_xy[0] == 3:
-                self.piece_xy[4] = self.piece_xy[type_num]
+                self.piece_xy[type_num + 1] = new_xy_txt
                 self.piece_xy[type_num] = 'x00'
+            else:
+                self.piece_xy[type_num] = new_xy_txt
         else:
             return self.Error()
     
-    def place(self, new_xy, type_num):
+    def place(self, new_xy, type_num, turn):
         pass
 
     def check_other(self, new_xy, type_num):
@@ -96,6 +117,7 @@ class BOARD:
             print('Change J -> H')
 
     def Player(self, command):
+        playing.turn = 0
         arr = [i for i in command]
         if arr[0] not in self.type_dic: print("Wrong command")
         if arr[1] in self.y_axis:
@@ -104,22 +126,31 @@ class BOARD:
             new_xy_num = (int(arr[2]), self.y_axis[arr[1]])
             self.move(new_xy_num, type_num, '0')
 
-    def Ai(self):
-        pass
+    def Ai(self, command):
+        arr = [i for i in command]
+        if arr[0] not in self.type_dic: print("Wrong command")
+        if arr[1] in self.y_axis:
+            type_num = self.type_dic[arr[0]] + 5
+            if self.piece_xy[type_num][0] == 'x': return
+            new_xy_num = (int(arr[2]), self.y_axis[arr[1]])
+            if arr[-1] == '!':
+                self.place(new_xy_num, type_num, '1')
+            self.move(new_xy_num, type_num, '1')
 
 if __name__ == '__main__':
     playing = BOARD()
     playing.reset_board()
     playing.print()
     while 1:
-        playing.turn = 0
+        
         command = input()
-        if command == '0':
+        if command == None:
             break
         playing.Player(command)
         playing.reset_board()
         playing.print()
         playing.turn = 1
-        playing.Ai()
+        command = input()
+        playing.Ai(command)
         playing.reset_board()
         playing.print()
