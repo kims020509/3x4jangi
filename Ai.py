d58aa1e2b106ae9fc75cdc4a6a7ab8c72bb0e263
ZiLA3 <kms-0509@naver.com>
2022-11-25 19:41:44 +0900

    15.221125
    계획

--- Ai.py
+++ Ai.py
@@ -4,7 +4,7 @@ import math
 Tree = np.array([])
 
 piece_score_dic = {'K' : 100 , 'S' : 3, 'J' : 3, 'H' : 2, 'Z' : 1}
-x_score_dic = {0 : 0.1, 1 : 0.2, 2 : 0.1, 3 : 0}
+x_score_dic = {0 : 0.1, 1 : 0.2, 2 : 0.2, 3 : 0.1}
 y_score_dic = {0 : -0.1 , 1 : 0, 2 : -0.1}
 
 class case:
@@ -20,7 +20,7 @@ class case:
                 own_score = 1 if piece[1] == '1' else -1
                 piece_socre = piece_score_dic[piece[0]]
                 special_socre = -1 if piece_socre == 2 else 1
-                xy_score = (1 + x_score_dic[(3 + j*own_score) % 4] * special_socre)*(1 + y_score_dic[i])
+                xy_score = (1 + x_score_dic[j] * special_socre)*(1 + y_score_dic[i])
                 score = piece_socre * xy_score * own_score
                 print(f'{piece}: {score:.2f}, ', end='')
                 total_score += score
