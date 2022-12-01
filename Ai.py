2c5e28afe7cad68a32d33b361625c8cc76f91e0d
ZiLA3 <kms-0509@naver.com>
2022-12-01 11:22:15 +0900

    18.221201
    minimax_1

--- Ai.py
+++ Ai.py
@@ -8,7 +8,6 @@ board = np.array([
     ['K0', 'Z0', 'Z1', 'K1'],
     ['J0', '--', '--', 'J1']])
 
-root = Node("root", data= None, expt= 0)
 type_dic = {'K' : 0, 'S' : 1, 'J' : 2, 'Z' : 3, 'H' : 4}
 piece_score_dic = {'K' : 100 , 'S' : 3, 'J' : 3, 'H' : 2, 'Z' : 1}
 x_score_dic = {0 : 0.1, 1 : 0.2, 2 : 0.2, 3 : 0.1}
@@ -37,7 +36,9 @@ def calc_expt(board):
             total_score += score
     print()
     return total_score
-            
+
+root = Node("root", data= board, expt= calc_expt(board))        
+
 def checkCASE(board):
     global Tree
     for y in range(3):
@@ -64,7 +65,7 @@ def checkCASE(board):
                     board[dy][dx] = tpiece
                     board[y][x] = piece
                     
-def minimax(board):
+def test(board):
     checkCASE(board)
 
     print("=="*20)
@@ -73,5 +74,65 @@ def minimax(board):
         print(f"{pre}{node.name}, expt: {node.expt:.2f}")
     print("=="*20)
 
+def minimax(node, depth, maximizingPlayer):
+    if depth == 0 : return node
+    minimax_value(node, depth, maximizingPlayer)
+    if len(root.children) == 0 : return 0
+    if maximizingPlayer:
+        bestValue = Node("Best", data= None, expt= -np.inf)
+        for child in node.children:
+            val = minimax(child, depth - 1, False)
+            if bestValue.expt < val.expt:
+                bestValue = val
+        return bestValue
+    else:
+        bestValue = Node("Best", data= None, expt= np.inf)
+        for child in node.children:
+            val = minimax(child, depth - 1, True)
+            if bestValue.expt > val.expt:
+                bestValue = val
+        return bestValue
+
+def minimax_value(node, depth, maximaizingPlayer):
+    print("==" * 22)
+    print(f"{depth} 단계 탐색")
+    print("==" * 22)
+
+    sf = int(maximaizingPlayer)
+    ot = int(not maximaizingPlayer)
+
+    for y in range(3):
+        for x in range(4):
+            piece = node.data[y][x]
+            if piece[0] == '-' : continue
+            if piece[1] == f'{ot}' : continue
+            ptype = type_dic[piece[0]] + 5*sf
+            nx = [1, 0, -1, 1, 0, -1, 1, 0, -1]
+            ny = [1, 1, 1, 0, 0, 0, -1, -1, -1]
+            for idx in range(9):
+                dx, dy = x + nx[idx], y +ny[idx]
+                if not -1 < dx < 4 or not -1 < dy < 3: continue 
+                tpiece = node.data[dy][dx]
+                if tpiece[1] == f'{sf}': continue
+                if db.piece_go[ptype][idx]:
+                    if ptype == 8 and dx < 2 :
+                        node.data[dy][dx] = 'H1'
+                    elif ptype == 3 and dx > 1:
+                        node.data[dy][dx] = 'H0'
+                    else:
+                        node.data[dy][dx] = piece
+                    node.data[y][x] = '--'
+                    print_(node.data)
+                    new_Node = Node(f"{chr(64+depth)}{ptype}{idx}", node, data= np.array(node.data), expt= calc_expt(node.data))
+                    node.data[dy][dx] = tpiece
+                    node.data[y][x] = piece
+
 if __name__ == '__main__':
-    minimax(board)
+    Best = minimax(root, 3, True)
+    print("=="*20)
+    for row in RenderTree(root):
+        pre, fill, node = row
+        print(f"{pre}{node.name}, expt: {node.expt:.2f}")
+    print("=="*20)
+
+    print(Best)
